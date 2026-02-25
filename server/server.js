6d5e5a58f86aa0cf1828c3de10ed2b0ae38d7f3a
JoysonStanly <joysonstanley3@gmail.com>
2026-02-25 20:04:30 +0530

    fix error in innges/index.js

--- server/server.js
+++ server/server.js
@@ -2,15 +2,26 @@ import express from 'express'
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from './inngest/index.js';
+import { serve } from "inngest/express";
 
 const app = express()
 
 app.use(express.json());
 app.use(cors());
 
-app.use(clerkMiddleware())
+// ✅ Skip Clerk for Inngest
+app.use((req, res, next) => {
+  if (req.path.startsWith("/api/inngest")) {
+    return next();
+  }
+  return clerkMiddleware()(req, res, next);
+});
 
 app.get("/", (req,res) => res.send("server is live"));
+
+// ✅ Inngest route
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000 
 
